83631c78c5ddf76999a563c1b3cd52bc117739cb
Duy Tai Nguyen <duytai.cse@gmail.com>
2018-09-29 15:33:47 +0800

    add testcase to home

--- src/ui/pages/Home/Home.test.js
+++ src/ui/pages/Home/Home.test.js
@@ -1,3 +1,13 @@
-it('should be ok', () => {
+import React from 'react'
+import { mount } from 'enzyme'
+import sinon from 'sinon'
+import Home from './Home'
 
+it('Home', () => {
+  const tweets = [{ message: ['hello', 'world'] }]
+  const addTweet = sinon.spy()
+  const wrapper = mount(<Home tweets={tweets} addTweet={addTweet}/>)
+  expect(wrapper.find('TweetComposer').length).toEqual(1)
+  expect(wrapper.find('Tweets').length).toEqual(1)
+  expect(wrapper.find('Tweets').find('p').length).toEqual(2)
 })
